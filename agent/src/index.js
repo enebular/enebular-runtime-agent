71ee5b78eadb0a764211b492c284e7f385f7efb1
Shinichi Tomita <shinichi.tomita@gmail.com>
2017-09-08 19:16:26 +0900

    change agent to support auth token request

--- agent/src/index.js
+++ agent/src/index.js
@@ -1,120 +1,147 @@
 /* @flow */
 import fs from 'fs';
 import path from 'path';
-import { spawn, exec } from 'child_process';
-import fetch from 'isomorphic-fetch';
-import type { ChildProcess } from 'child_process';
+import os from 'os';
+import EventEmitter from 'events';
+import NodeREDController from './node-red-controller';
+import DeviceAuthMediator from './device-auth-mediator';
+import AgentManagerMediator from './agent-manager-mediator';
 
 /**
  *
  */
-type EnebularAgentConfig = {
-  command: string,
-  args: string[],
-  pkgDir: string,
+export type EnebularAgentConfig = {
+  nodeRedDir: string,
+  nodeRedCommand?: string,
+  configFile: string,
 };
 
-type NodeRedFlowPackage = {
-  flows: Object[],
-  creds: Object,
-  packages: Object,
-};
+export type AgentState =
+  'init' |
+  'registered' |
+  'unregistered' |
+  'authenticated' |
+  'unauthenticated'
+;
+
+/**
+ *
+ */
+function isPossibleStateTransition(state: AgentState, nextState: AgentState) {
+  switch (state) {
+    case 'init':
+      return nextState === 'registered' || nextState === 'unregistered';
+    case 'registered':
+      return nextState === 'authenticated' || nextState === 'unauthenticated';
+    case 'unregistered':
+      return nextState === 'registered';
+    case 'authenticated':
+      return nextState === 'unauthenticated';
+    case 'unauthenticated':
+      return nextState === 'authenticated';
+  }
+}
 
 /**
  *
  */
 export default class EnebularAgent {
-  _command: string;
-  _args: string[];
-  _pkgDir: string;
-  _cproc: ?ChildProcess = null;
+  _configFile: string;
 
-  constructor({ command, args, pkgDir }: EnebularAgentConfig) {
-    this._command = command;
-    this._args = args;
-    this._pkgDir = pkgDir;
+  _messageEmitter: EventEmitter;
+  _nodeRed: NodeREDController;
+  _deviceAuth: DeviceAuthMediator;
+  _agentMan: AgentManagerMediator;
+
+  _connectionId: ?string;
+  _deviceId: ?string;
+
+  _agentState: AgentState;
+
+  constructor(config: EnebularAgentConfig) {
+    super();
+    const {
+      nodeRedDir,
+      nodeRedCommand = 'npm start',
+      configFile = path.join(os.homedir(), '.enebular-config.json'),
+    } = config;
+    this._messageEmitter = new EventEmitter();
+    this._nodeRed = new NodeREDController(nodeRedDir, nodeRedCommand, this._messageEmitter);
+    this._deviceAuth = new DeviceAuthMediator(this._messageEmitter);
+    this._agentMan = new AgentManagerMediator();
+    this._configFile = configFile;
+    this._agentState = 'init';
+    this._loadAgentConfig();
   }
 
-  async downloadAndUpdatePackage(downloadUrl: string) {
-    const res = await fetch(downloadUrl);
-    if (res.status >= 400) {
-      throw new Error('invalid url');
-    }
-    const body = await res.json();
-    return this.updatePackage(body);
-  }
-
-  async updatePackage(flowPackage: NodeRedFlowPackage) {
-    const updates = [];
-    if (flowPackage.flow || flowPackage.flows) {
-      const flows = flowPackage.flow || flowPackage.flows;
-      updates.push(
-        new Promise((resolve, reject) => {
-          const flowFilePath = path.join(this._pkgDir, '.node-red-config', 'flows.json');
-          fs.writeFile(flowFilePath, JSON.stringify(flows), (err) => err ? reject(err) : resolve());
-        })
-      );
-    }
-    if (flowPackage.cred || flowPackage.creds) {
-      const creds = flowPackage.cred || flowPackage.creds;
-      updates.push(
-        new Promise((resolve, reject) => {
-          const credFilePath = path.join(this._pkgDir, '.node-red-config', 'flows_cred.json');
-          fs.writeFile(credFilePath, JSON.stringify(creds), (err) => err ? reject(err) : resolve());
-        })
-      );
-    }
-    if (flowPackage.packages) {
-      updates.push(
-        new Promise((resolve, reject) => {
-          const packageJSONFilePath = path.join(this._pkgDir, '.node-red-config', 'enebular-agent-dynamic-deps', 'package.json');
-          const packageJSON = JSON.stringify({
-            name: 'enebular-agent-dynamic-deps',
-            version: '0.0.1',
-            dependencies: flowPackage.packages,
-          }, null, 2);
-          fs.writeFile(packageJSONFilePath, packageJSON, (err) => err ? reject(err) : resolve());
-        })
-      );
-    }
-    await Promise.all(updates);
-    await this.resolveDependency();
-  }
-
-  async resolveDependency() {
-    return new Promise((resolve, reject) => {
-      const cproc = spawn('npm', [ 'install', 'enebular-agent-dynamic-deps' ], { stdio: 'inherit', cwd: this._pkgDir });
-      cproc.on('error', reject);
-      cproc.once('exit', resolve);
-    });
-  }
-
-  async startService() {
-    return new Promise((resolve, reject) => {
-      const cproc = spawn(this._command, this._args, { stdio: 'inherit', cwd: this._pkgDir });
-      cproc.on('error', reject);
-      cproc.once('exit', resolve);
-      this._cproc = cproc;
-    });
-  }
-
-  async shutdownService() {
-    return new Promise((resolve, reject) => {
-      const cproc = this._cproc;
-      if (cproc) {
-        cproc.kill();
-        cproc.once('exit', () => {
-          this._cproc = null;
-          resolve();
-        });
-      } else {
-        resolve();
+  _loadAgentConfig() {
+    try {
+      const data = fs.readFileSync(this._configFile, 'utf8');
+      const { connectionId, deviceId, agentManagerBaseUrl, authRequestUrl } = JSON.parse(data);
+      if (connectionId && deviceId && agentManagerBaseUrl && authRequestUrl) {
+        this._connectionId = deviceId;
+        this._deviceId = deviceId;
+        this._deviceAuth.setAuthRequestUrl(authRequestUrl);
+        this._agentMan.setBaseUrl(agentManagerBaseUrl);
+        this._changeAgentState('registered');
       }
-    });
+    } catch (e) {
+      console.error(e);
+      this._changeAgentState('unregistered');
+    }
   }
 
-  async restartService() {
-    await this.shutdownService();
-    await this.startService();
+  _changeAgentState(nextState: AgentState) {
+    if (isPossibleStateTransition(this._agentState, nextState)) {
+      this._agentState = nextState;
+      console.log(`*** agent state : ${this._agentState} ***`);
+      try {
+        this._handleChangeState();
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      console.warn(`Impossible state transition requested : ${this._agentState} => ${nextState}`);
+    }
   }
+
+  async _handleChangeState() {
+    switch (this._agentState) {
+      case 'registered':
+        await this._requestDeviceAuthentication();
+        break;
+      case 'unregistered':
+        break;
+      case 'authenticated':
+        await this._startStatusNotification();
+        break;
+    }
+  }
+
+  async _requestDeviceAuthentication() {
+    const { _connectionId: connectionId, _deviceId: deviceId } = this;
+    if (!connectionId || !deviceId) {
+      throw new Error('Connection ID and Device ID are not configured yet for the agent');
+    }
+    try {
+      const { accessToken } = await this._deviceAuth.requestAuthenticate(connectionId, deviceId);
+      this._agentMan.setAccessToken(accessToken);
+      this._changeAgentState('authenticated');
+    } catch (err) {
+      this._changeAgentState('unauthenticated');
+      throw err;
+    }
+  }
+
+  async _startStatusNotification() {
+    this._agentMan.startStatusReport();
+  }
+
+  /**
+   *
+   */
+  handleDeviceMasterMessage(messageType: string, message: any) {
+    this._messageEmitter.emit(messageType, message);
+  }
+
 }
