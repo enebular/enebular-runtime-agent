f0ada03422cdbde3569291930179035cc6a93675
Shinichi Tomita <shinichi.tomita@gmail.com>
2017-09-11 18:01:54 +0900

    fix node-red controller method name

--- agent/src/node-red-controller.js
+++ agent/src/node-red-controller.js
@@ -170,7 +170,7 @@ export default class NodeREDController {
     });
   }
 
-  async shutdownService() {
+  async restartService() {
     return this._queueAction(() => this._restartService());
   }
 
