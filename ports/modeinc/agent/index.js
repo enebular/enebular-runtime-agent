a8bfdc32d64d090104447523e19b9ae9fb0018d3
Shinichi Tomita <shinichi.tomita@gmail.com>
2017-09-11 13:16:26 +0900

    update mode-inc agent to use agent runtime 2.0

--- ports/modeinc/agent/index.js
+++ ports/modeinc/agent/index.js
@@ -1,39 +1,22 @@
+/* @flow */
 import ModeDevice from 'mode-device';
-import EnebularAgent from 'enebular-runtime-agent';
+import EnebularAgent from '@uhuru/enebular-runtime-agent';
 
 const { DEVICE_ID, DEVICE_API_KEY } = process.env;
 
 const device = new ModeDevice(DEVICE_ID, DEVICE_API_KEY);
 const agent = new EnebularAgent({
-  command: 'npm',
-  args: ['run', 'start' ],
-  pkgDir: '../../../node-red',
+  nodeRedDir: '../../../node-red',
+  configFile: './.enebular-config.json',
 });
-
-async function fetchAndUpdateFlow(params) {
-  await agent.downloadAndUpdatePackage(params.downloadUrl);
-  await agent.restartService();
-}
 
 async function startup() {
   device.commandCallback = (msg, flags) => {
     console.log(msg, flags);
-    switch(msg.action) {
-      case 'update-flow':
-        fetchAndUpdateFlow(msg.parameters);
-        break;
-      case 'restart':
-        agent.restartService();
-        break;
-      case 'shutdown':
-        agent.shutdownService();
-        break;
-      default:
-        break;
-    }
+    agent.handleDeviceMasterMessage(msg.action, msg.parameters);
   };
   device.listenCommands();
-  const ret = await agent.startService();
+  await agent.start();
   console.log('agent started up');
 }
 
