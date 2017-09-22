f06ec88de72b309d87f6ce08a6a6d6bc7d430ff5
Shinichi Tomita <shinichi.tomita@gmail.com>
2017-09-22 11:27:03 +0900

    update awsiot to support agent 2.0

--- ports/awsiot/src/index.js
+++ ports/awsiot/src/index.js
@@ -1,85 +1,127 @@
+/* @flow */
 import fs from 'fs';
+import path from 'path';
 import awsIot from 'aws-iot-device-sdk';
-import fetch from 'isomorphic-fetch';
+import debug from 'debug';
 import EnebularAgent from 'enebular-runtime-agent';
 
-const agent = new EnebularAgent({
-  command: 'npm',
-  args: ['run', 'start' ],
-  pkgDir: process.env.NODE_RED_DIR,
-});
 
-let device;
+/**
+ *
+ */
+const log = debug('enebular-awsiot-agent');
 
-async function fetchAndUpdateFlow(params) {
-  console.log('fetchAndUpdateFlow', params);
-  await agent.downloadAndUpdatePackage(params.downloadUrl);
-  await agent.restartService();
+const { AWSIOT_CONFIG_FILE, NODE_RED_DIR } = process.env;
+
+/**
+ *
+ */
+function isThingShadowSynced(metadata, property) {
+  const desiredTimestamp = (metadata.desired && metadata.desired[property] || {}).timestamp;
+  const reportedTimestamp = (metadata.reported && metadata.reported[property] || {}).timestamp;
+  return reportedTimestamp >= desiredTimestamp;
 }
 
-function updateThingState(thingName, state) {
-  device.update(thingName, { state: { reported: state } });
-}
+/**
+ *
+ */
+export type AWSIoTConfig = {
+  thingName: string,
+};
 
-function setupDevice(config) {
+function setupDevice(config: AWSIoTConfig, agent: EnebularAgent) {
   const device = awsIot.thingShadow(config);
 
   device.on('connect', async () => {
-    console.log('>> connected to AWS IoT');
+    log('>> connected to AWS IoT');
     device.register(config.thingName, { ignoreDeltas: false, persistentSubscribe: true });
     setTimeout(() => device.get(config.thingName), 2000);
-    await agent.restartService();
   });
 
   device.on('close', () => {
-    console.log('>> AWS IoT connection closed');
+    log('>> AWS IoT connection closed');
     device.unregister(config.thingName);
   });
 
   device.on('reconnect', () => {
-    console.log('>> reconnect to AWS IoT');
+    log('>> reconnect to AWS IoT');
     device.register(config.thingName);
   });
 
   device.on('error', (error) => {
-    console.log('## error', error);
+    log('## error', error);
   });
 
   device.on('offline', () => {
-    console.log('>> offline : no AWS IoT connection established');
+    log('>> offline : no AWS IoT connection established');
+  });
+
+  device.on('message', (topic, payload) => {
+    log('>> message', topic, payload);
   });
 
   device.once('status', async (thingName, stat, clientToken, stateObject) => {
-    var state = stateObject.state;
-    var metadata = stateObject.metadata;
-    if (state.desired.package) {
-      await fetchAndUpdateFlow(state.desired.package);
-      updateThingState(config.thingName, { package: state.desired.package });
+    log('>> status', stateObject);
+    const state = stateObject.state;
+    const metadata = stateObject.metadata;
+    if (state.desired.message && !isThingShadowSynced(metadata, 'message')) {
+      const { messageType, message } = state.desired.message;
+      agent.handleDeviceMasterMessage(messageType, message);
+      const newState = { message: state.desired.message };
+      device.update(thingName, { state: { reported: newState } });
     }
   });
 
   device.on('delta', async (thingName, stateObject) => {
-    var state = stateObject.state;
-    var metadata = stateObject.metadata;
-    if (state.package) {
-      await fetchAndUpdateFlow(state.package);
-      updateThingState(config.thingName, { package: state.package });
+    log('>> delta', stateObject);
+    const state = stateObject.state;
+    const metadata = stateObject.metadata;
+    if (state.message && !isThingShadowSynced(metadata, 'message')) {
+      const { messageType, message } = state.message;
+      agent.handleDeviceMasterMessage(messageType, message);
+      const newState = { message: state.message };
+      device.update(thingName, { state: { reported: newState } });
     }
   });
-
-  return device;
 }
 
-export function startup() {
-  console.log(process.argv);
-  const configFile = process.env.AWSIOT_CONFIG_FILE || process.argv[2] || './config.json';
-  console.log('configFile=', configFile);
-  const config = JSON.parse(fs.readFileSync(configFile));
-  console.log('awsiot config=>', config);
-  device = setupDevice(config);
-  console.log('agent started up');
+
+let agent: EnebularAgent;
+
+/**
+ *
+ */
+async function startup() {
+  try {
+    agent = new EnebularAgent({
+      nodeRedDir: NODE_RED_DIR || path.join(process.cwd(), 'node-red'),
+      configFile: path.join(process.cwd(), '.enebular-config.json'),
+    });
+
+    const awsIoTConfigFile = AWSIOT_CONFIG_FILE || path.join(process.cwd(), './config.json');
+    log('AWS IoT config file =', awsIoTConfigFile);
+    const awsIotConfig = JSON.parse(fs.readFileSync(awsIoTConfigFile, 'utf8'));
+    setupDevice(awsIotConfig, agent);
+
+    await agent.startup();
+    log('### enebular agent started up ####');
+    return agent;
+
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+/**
+ *
+ */
+async function shutdown() {
+  return agent.shutdown();
 }
 
 if (require.main === module) {
   startup();
 }
+
+export { startup, shutdown };
