0e63205e42965ce5776e7c9b420d524b16230f1d
Chris McHarg <chris@sporelab.io>
2017-12-06 15:44:06 +1300

    mode: update to latest core interface

--- ports/modeinc/src/index.js
+++ ports/modeinc/src/index.js
@@ -1,12 +1,13 @@
 /* @flow */
 import path from 'path';
 import ModeDevice from 'mode-device';
-import EnebularAgent from 'enebular-runtime-agent';
+import { EnebularAgent, MessengerService } from 'enebular-runtime-agent';
 
 const { DEVICE_ID, DEVICE_API_KEY, NODE_RED_DIR } = process.env;
 
+const messenger = new MessengerService();
 const device = new ModeDevice(DEVICE_ID, DEVICE_API_KEY);
-const agent = new EnebularAgent({
+const agent = new EnebularAgent(messenger, {
   nodeRedDir: NODE_RED_DIR || path.join(process.cwd(), 'node-red'),
   configFile: path.join(process.cwd(), '.enebular-config.json'),
 });
@@ -15,11 +16,12 @@ async function startup() {
   try {
     device.commandCallback = (msg, flags) => {
       console.log(msg, flags);
-      agent.handleDeviceMasterMessage(msg.action, msg.parameters);
+      messenger.sendMessage(msg.action, msg.parameters);
     };
     device.listenCommands();
     await agent.startup();
     console.log('### enebular agent started up ####');
+    messenger.updateConnectedState(true);
     return agent;
   } catch (err) {
     console.error(err);
