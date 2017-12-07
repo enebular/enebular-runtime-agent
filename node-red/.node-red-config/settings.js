5211d9d8d6970b765310c77d8f2866ca1863990c
Alex Wong <chantlong@gmail.com>
2017-12-07 10:58:21 +0900

    tidy up node-red-config and aws config.json

--- node-red/.node-red-config/settings.js
+++ node-red/.node-red-config/settings.js
@@ -3,30 +3,7 @@ const path = require('path')
 
 module.exports = {
   userDir: __dirname,
-  flowFile: 'flows.json',
-  // logging: {
-  //   console: {
-  //       level: "info",
-  //       metrics: false,
-  //       audit: false
-  //   },
-  //   logstash: {
-  //       level:'info',
-  //       metrics:true,
-  //       handler: function(conf) {
-  //           return function(msg) {
-  //               try {
-  //                 if (msg.msg || msg.msg && msg.type) {
-  //                   delete msg['level']
-  //                   fs.appendFile(path.resolve(__dirname, './../../ports/' ) + , JSON.stringify(msg)+"\n", function (err) {
-  //                     if (err) throw err;
-  //                   }); 
-  //                 }
-  //               }catch(err) { console.log(err);}
-  //           }
-  //       }
-  //   }
-  // }
+  flowFile: 'flows.json'
 };
 
 
