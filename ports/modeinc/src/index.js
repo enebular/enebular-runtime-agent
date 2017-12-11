721f9d415fbfe431fdeb2f6f349463f68b97f329
Chris McHarg <chris@sporelab.io>
2017-12-11 14:36:27 +1300

    mode: logging improvements

--- ports/modeinc/src/index.js
+++ ports/modeinc/src/index.js
@@ -1,9 +1,13 @@
 /* @flow */
 import path from 'path'
 import ModeDevice from 'mode-device'
+import debug from 'debug'
 import { EnebularAgent, MessengerService } from 'enebular-runtime-agent'
 
 const { DEVICE_ID, DEVICE_API_KEY, NODE_RED_DIR } = process.env
+
+const log = debug('enebular-mode-agent')
+const logv = debug('enebular-mode-agent:verbose')
 
 const messenger = new MessengerService()
 const device = new ModeDevice(DEVICE_ID, DEVICE_API_KEY)
@@ -15,13 +19,14 @@ const agent = new EnebularAgent(messenger, {
 async function startup () {
   try {
     device.commandCallback = (msg, flags) => {
+      logv('Mode command: ' + JSON.stringify(msg))
+      log('Message: ' + msg.action)
       messenger.sendMessage(msg.action, msg.parameters)
     }
     device.listenCommands()
     await agent.startup()
-    console.log('### mode enebular agent started up ####')
+    log('Agent started')
     messenger.updateConnectedState(true)
-    return agent
   } catch (err) {
     console.error(err)
     process.exit(1)
