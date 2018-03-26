1a2e22819e73f2a0934595836779772667bb1a2c
Chris McHarg <chris@sporelab.io>
2018-03-27 10:26:41 +1300

    local: update to use prettier (with uhuru standards)

--- ports/local/src/index.js
+++ ports/local/src/index.js
@@ -1,15 +1,16 @@
 /* @flow */
-import net from 'net';
-import fs from 'fs';
-import path from 'path';
-import { EnebularAgent, MessengerService } from 'enebular-runtime-agent';
+import net from 'net'
+import fs from 'fs'
+import path from 'path'
+import { EnebularAgent, MessengerService } from 'enebular-runtime-agent'
 
 const MODULE_NAME = 'local'
-const END_OF_MSG_MARKER = 0x1E; // RS (Record Separator)
-const SOCKET_PATH = process.env.SOCKET_PATH || '/tmp/enebular-local-agent.socket';
+const END_OF_MSG_MARKER = 0x1e // RS (Record Separator)
+const SOCKET_PATH =
+  process.env.SOCKET_PATH || '/tmp/enebular-local-agent.socket'
 
-let agent: EnebularAgent;
-let localServer: net.Server;
+let agent: EnebularAgent
+let localServer: net.Server
 
 function log(level: string, msg: string, ...args: Array<mixed>) {
   args.push({ module: MODULE_NAME })
@@ -30,134 +31,131 @@ function error(msg: string, ...args: Array<mixed>) {
 
 function attemptSocketRemove() {
   try {
-    fs.unlinkSync(SOCKET_PATH);
+    fs.unlinkSync(SOCKET_PATH)
   } catch (err) {
     // ignore any errors
   }
 }
 
 async function startLocalServer(messenger: MessengerService): net.Server {
-
   function handleClientMessage(clientMessage: string) {
-    debug(`client message: [${clientMessage}]`);
-    let message;
+    debug(`client message: [${clientMessage}]`)
+    let message
     try {
-      message = JSON.parse(clientMessage);
+      message = JSON.parse(clientMessage)
       switch (message.type) {
         case 'connect':
-          messenger.updateConnectedState(true);
-        break;
+          messenger.updateConnectedState(true)
+          break
         case 'disconnect':
-          messenger.updateConnectedState(false);
-        break;
+          messenger.updateConnectedState(false)
+          break
         case 'message':
           messenger.sendMessage(
             message.message.messageType,
             message.message.message
-          );
-        break;
+          )
+          break
         default:
-          info('unsupported client message type: ' + message.type);
-        break;
+          info('unsupported client message type: ' + message.type)
+          break
       }
     } catch (err) {
-      error('client message: failed to handle: ' + err);
+      error('client message: failed to handle: ' + err)
     }
   }
 
-  const server = net.createServer((socket) => {
+  const server = net.createServer(socket => {
+    info('client connected')
 
-    info('client connected');
+    socket.setEncoding('utf8')
 
-    socket.setEncoding('utf8');
+    let message = ''
 
-    let message = '';
-
-    socket.on('data', (data) => {
-      message += data;
-      if (message.charCodeAt(message.length-1) === END_OF_MSG_MARKER) {
-        message = message.slice(0, -1);
-        handleClientMessage(message);
-        message = '';
+    socket.on('data', data => {
+      message += data
+      if (message.charCodeAt(message.length - 1) === END_OF_MSG_MARKER) {
+        message = message.slice(0, -1)
+        handleClientMessage(message)
+        message = ''
       }
-    });
+    })
 
     socket.on('end', () => {
       if (message.length > 0) {
-        info('client ended with partial message: ' + message);
-        message = '';
+        info('client ended with partial message: ' + message)
+        message = ''
       }
-    });
+    })
 
     socket.on('close', () => {
-      info('client disconnected');
-      messenger.updateConnectedState(false);
-    });
+      info('client disconnected')
+      messenger.updateConnectedState(false)
+    })
 
-    socket.on('error', (err) => {
-      info('client socket error: ' + err);
-    });
+    socket.on('error', err => {
+      info('client socket error: ' + err)
+    })
 
-    socket.write('ok' + String.fromCharCode(END_OF_MSG_MARKER));
-
-  });
+    socket.write('ok' + String.fromCharCode(END_OF_MSG_MARKER))
+  })
 
   server.on('listening', () => {
-    info('server listening on: ' + JSON.stringify(server.address()));
-  });
+    info('server listening on: ' + JSON.stringify(server.address()))
+  })
 
-  server.on('error', (err) => {
-    error('server error: ' + err);
-  });
+  server.on('error', err => {
+    error('server error: ' + err)
+  })
 
   server.on('close', () => {
-    info('server closed');
-  });
+    info('server closed')
+  })
 
-  attemptSocketRemove();
-  server.listen(SOCKET_PATH);
+  attemptSocketRemove()
+  server.listen(SOCKET_PATH)
 
-  return server;
+  return server
 }
 
 async function startup() {
-
-  const messenger = new MessengerService();
+  const messenger = new MessengerService()
 
   agent = new EnebularAgent(messenger, {
-    nodeRedDir: process.env.NODE_RED_DIR || path.join(process.cwd(), 'node-red'),
-    configFile: path.join(process.cwd(), '.enebular-config.json'),
-  });
+    nodeRedDir:
+      process.env.NODE_RED_DIR || path.join(process.cwd(), 'node-red'),
+    configFile: path.join(process.cwd(), '.enebular-config.json')
+  })
 
-  await agent.startup();
-  info('agent started');
+  await agent.startup()
+  info('agent started')
 
-  localServer = await startLocalServer(messenger);
+  localServer = await startLocalServer(messenger)
 }
 
 async function shutdown() {
-  await localServer.close();
-  attemptSocketRemove();
-  return agent.shutdown();
+  await localServer.close()
+  attemptSocketRemove()
+  return agent.shutdown()
 }
 
 async function exit() {
-  await shutdown();
-  process.exit(0);
+  await shutdown()
+  process.exit(0)
 }
 
 if (require.main === module) {
-  startup();
+  startup()
   process.on('SIGINT', () => {
-    exit();
-  });
+    exit()
+  })
   process.on('SIGTERM', () => {
-    exit();
-  });
-  process.on('uncaughtException', (err) => {
-    console.error(`Uncaught exception: ${err.stack}`);
-    process.exit(1);
-  });
+    exit()
+  })
+  process.on('uncaughtException', err => {
+    console.error(`Uncaught exception: ${err.stack}`)
+    process.exit(1)
+  })
 }
 
-export { startup, shutdown };
+export { startup, shutdown }
