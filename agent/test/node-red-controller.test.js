5531199d55790720bd929e7f0b0304668741e925
Youxin Su <suyouxin@sporelab.io>
2018-07-18 11:31:13 +1200

    test: increase Node-RED initial time out

--- agent/test/node-red-controller.test.js
+++ agent/test/node-red-controller.test.js
@@ -89,7 +89,7 @@ async function createAgentRunningWithTestNodeRedSettings(
   connector = ret.connector
 
   // console.log("user directory: ", agent._nodeRed._getDataDir())
-  t.true(await nodeRedIsAlive(NodeRedPort, 3000))
+  t.true(await nodeRedIsAlive(NodeRedPort, 5000))
 }
 
 test.serial(
