214f05168bf518c640d79ec6a641c77a9e4600a6
Youxin Su <suyouxin@sporelab.io>
2018-11-13 10:29:28 +1300

    local port: move local port into agent

--- agent/src/index.js
+++ agent/src/index.js
@@ -1,5 +1,6 @@
 /* @flow */
 import EnebularAgent from './enebular-agent'
 import ConnectorService from './connector-service'
+import LocalPort from './local-port'
 
-export { EnebularAgent, ConnectorService }
+export { EnebularAgent, ConnectorService, LocalPort }
