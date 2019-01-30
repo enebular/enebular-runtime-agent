cd2bb88894b200930d0976615a6c116352f7a019
Youxin Su <suyouxin@sporelab.io>
2019-01-30 14:53:03 +1300

    install: added --aws-iot-disable-rule-creation option

--- tools/install/install.sh
+++ tools/install/install.sh
@@ -788,7 +788,8 @@ post_install() {
   fi
   if [ ! -z ${AWS_IOT_THING_NAME} ] && [ ${PORT} == 'awsiot' ]; then
     _task Creating AWS IoT thing
-      cmd_wrapper run_as_user ${USER} "(cd ${INSTALL_DIR}/tools/awsiot-thing-creator && npm run start)" "${NODE_ENV_PATH} \
+    cmd_wrapper run_as_user ${USER} "(cd ${INSTALL_DIR}/tools/awsiot-thing-creator && npm run start)" "${NODE_ENV_PATH} \
+        DISABLE_RULE_CREATION=${AWS_IOT_DISABLE_RULE_CREATION} \
         AWS_IOT_THING_NAME=${AWS_IOT_THING_NAME} AWS_IOT_REGION=${AWS_IOT_REGION} \
         AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}"
     EXIT_CODE=$?
@@ -831,6 +832,7 @@ AGENT_DOWNLOAD_PATH="https://api.github.com/repos/enebular/enebular-runtime-agen
 SUPPORTED_NODE_VERSION="v9.2.1"
 ENEBULAR_BASE_URL="https://enebular.com/api/v1"
 MBED_CLOUD_MODE=develop
+AWS_IOT_DISABLE_RULE_CREATION=false
 
 LOG_FILE="$(create_log)"
 chmod +r ${LOG_FILE}
@@ -876,6 +878,10 @@ case $i in
   ;;
   --aws-iot-thing-name=*)
   AWS_IOT_THING_NAME="${i#*=}"
+  shift
+  ;;
+  --aws-iot-disable-rule-creation)
+  AWS_IOT_DISABLE_RULE_CREATION=true
   shift
   ;;
   --mbed-cloud-dev-cred=*)
