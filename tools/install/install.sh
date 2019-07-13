0cec835e0c59978d09d82b7aa6380dfd3970efaa
Youxin Su <suyouxin@sporelab.io>
2019-07-13 16:57:48 +1200

    Specify python relevant path while running pip & python

--- tools/install/install.sh
+++ tools/install/install.sh
@@ -724,9 +724,9 @@ do_install() {
 setup_mbed_cloud_connector_fcc() {
   _task "Deploying mbed project"
   local LOCAL_BIN_ENV
-  LOCAL_BIN_ENV="PATH=/home/${USER}/.local/bin:${PATH}"
+  LOCAL_BIN_ENV="PATH=/home/${USER}/.local/bin:${PATH} PYTHONUSERBASE=/home/${USER}/.local PYTHONPATH=/usr/lib/python2.7"
   cmd_wrapper run_as_user ${USER} "(cd ${INSTALL_DIR}/tools/mbed-cloud-connector-fcc \
-    && mbed config root . && mbed deploy -v)" ${LOCAL_BIN_ENV}
+    && mbed config root . && mbed deploy -v)" "${LOCAL_BIN_ENV}"
   EXIT_CODE=$?
   if [ "$EXIT_CODE" -ne 0 ]; then
     _err "mbed deploy failed."
--- tools/updater/src/system.ts
+++ tools/updater/src/system.ts
@@ -301,13 +301,17 @@ export class System implements SystemIf {
   public async installPythonPackages(packages: string[], userInfo: UserInfo): Promise<void> {
     return new Promise(
       async (resolve, reject): Promise<void> => {
+        let pipEnv: NodeJS.ProcessEnv = {}
+        pipEnv['PYTHONUSERBASE'] = `/home/${userInfo.user}/.local`
+        pipEnv['PYTHONPATH'] = `/usr/lib/python2.7`
         let options = ['install']
         options = options.concat(packages)
         options.push("--user")
         try {
           await Utils.spawn('pip', options, this._log, {
             uid: userInfo.uid,
-            gid: userInfo.gid
+            gid: userInfo.gid,
+            env: pipEnv
           })
           this._pipRetryCount = 0
           resolve()
