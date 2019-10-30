e307e67eaee7452fc794b228e3c0f25ca4d08e3c
junichi.katsu <junichi.katsu@uhuru.jp>
2019-10-30 06:22:24 +0000

    #346 pipeline test

--- tools/s3_download.py
+++ tools/s3_download.py
@@ -5,9 +5,9 @@ import argparse
 import boto3
 from botocore.exceptions import ClientError
 
-def download_from_s3(bucket_location, bucket_key):
+def download_from_s3(bucket_location):
     print("bucket_location:",bucket_location)
-    print("bucket_key:",bucket_key)
+#    print("bucket_key:",bucket_key)
     s3 = boto3.resource('s3')
 
     bucket = s3.Bucket('development')
@@ -21,10 +21,10 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("bucket", help="Name of the existing S3 bucket location")
-    parser.add_argument("bucket_key", help="Name of the S3 Bucket key")
+#    parser.add_argument("bucket_key", help="Name of the S3 Bucket key")
     args = parser.parse_args()
 
-    if not download_from_s3(args.bucket, args.bucket_key):
+    if not download_from_s3(args.bucket):
         sys.exit(1)
 
 if __name__ == "__main__":
