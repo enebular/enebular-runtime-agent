afd9a86d29412fbfa41e5a971cf49a38cf3b8899
junichi.katsu <junichi.katsu@uhuru.jp>
2019-10-30 12:36:25 +0000

    #346 pipeline test

--- tools/s3_download.py
+++ tools/s3_download.py
@@ -3,6 +3,7 @@ import os
 import sys
 import argparse
 import boto3
+from boto3 import Session
 from botocore.exceptions import ClientError
 import subprocess
 
@@ -12,7 +13,7 @@ def download_from_s3(bucket_location):
     s3 = boto3.resource('s3')
 
     bucket = s3.Bucket('enebular-world')
-    objects = bucket.objects('development/sign-key-pair/latest').all()
+    objects = bucket.objects(Prefix='development/sign-key-pair/latest/').all()
     for a_object in objects:
         print(a_object)
     bucket.download_file('development/sign-key-pair/latest/5b1001a0-f2b8-4098-84be-1d7254a6ce70.pub','agent/keys/enebular/5b1001a0-f2b8-4098-84be-1d7254a6ce70.pub' )
