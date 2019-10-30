1564fd4d7137f2e03dec1136f64f63bd568006a8
junichi.katsu <junichi.katsu@uhuru.jp>
2019-10-30 06:36:43 +0000

    #346 pipeline test

--- tools/s3_download.py
+++ tools/s3_download.py
@@ -4,27 +4,29 @@ import sys
 import argparse
 import boto3
 from botocore.exceptions import ClientError
+import subprocess
 
-def download_from_s3(bucket_location):
+def download_from_s3(bucket_location, bucket_key):
     print("bucket_location:",bucket_location)
-#    print("bucket_key:",bucket_key)
+    print("bucket_key:",bucket_key)
     s3 = boto3.resource('s3')
 
     bucket = s3.Bucket('development')
-    objects = bucket.objects.all()
-    for a_object in objects:
-        print(a_object)
-    # bucket.download_file('fuga.txt', 'fuga.txt')
+    #objects = bucket.objects.all()
+    #for a_object in objects:
+    #    print(a_object)
+    bucket.download_file('sign-key-pair/latest/5b1001a0-f2b8-4098-84be-1d7254a6ce70.pub', '5b1001a0-f2b8-4098-84be-1d7254a6ce70.pub')
+    print(subprocess.run(["ls", "-l", "/tmp"], stdout=subprocess.PIPE))
     return True
 
 def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("bucket", help="Name of the existing S3 bucket location")
-#    parser.add_argument("bucket_key", help="Name of the S3 Bucket key")
+    parser.add_argument("bucket_key", help="Name of the S3 Bucket key")
     args = parser.parse_args()
 
-    if not download_from_s3(args.bucket):
+    if not download_from_s3(args.bucket, args.bucket_key):
         sys.exit(1)
 
 if __name__ == "__main__":
